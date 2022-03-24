fd9a43b3404a4e38c1cf0848924bbe8a47ddc2b5
FabianAndersson17 <89392496+FabianAndersson17@users.noreply.github.com>
2022-03-24 16:23:29 +0100

    Almost done with production model

--- Labs/production_model.py
+++ Labs/production_model.py
@@ -11,11 +11,12 @@ test_samples = test_samples.drop(columns=["BMI_class", "Blood_pressure_class", "
 X_test, y_test = test_samples.drop(columns="cardio", axis=1), test_samples["cardio"]
 y_pred = model.predict(X_test)
 
-predictions = pd.DataFrame(y_pred)
-predictions = predictions.rename({0: "prediction"}, axis=1)
 predict_probability = model.predict_proba(X_test)
 print(predict_probability) 
 print(y_pred)
+predictions = pd.DataFrame([y_pred, predict_probability]).T
+predictions = predictions.rename({0: "prediction", 1: "Probability"}, axis=1)
+print(predictions)
 ## Gives a list of lists with 1 and 0. This happens because the decision tree can either go "left" or "rigth" 
 ## Dependent on if the values of the data point are according to the parameters of the left path or right path
 ## https://stackoverflow.com/questions/47251594/scikit-learn-decision-tree-probability-of-prediction-being-a-or-b?msclkid=77315ed3ab8211ec9f236be39ea50843
